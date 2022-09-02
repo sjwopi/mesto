69b463776db5ef0248f8c7277182f42ed618dedf
Ivan Teteruk <iteterk@gmail.com>
2022-09-02 08:28:45 -0200

    сделать секцию elements

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let content = document.querySelector(".content");
 let editButton = content.querySelector(".profile__edit-btn");
-let closeButton = content.querySelector(".popup__close-btn")
+let closeButton = content.querySelector(".popup__close-btn");
+
 
 function editPopUp() {
     let popUp = content.querySelector(".popup");
@@ -8,18 +9,3 @@ function editPopUp() {
 }
 editButton.addEventListener("click", editPopUp);
 closeButton.addEventListener("click", editPopUp);
-
-
-/* let content = document.querySelector('.content');
-    editText.setAttribute("style", "opacity: 1;")
-
-let addButton = container.querySelector('.')
-
-if (songs.length === 0) {
-    resetButton.setAttribute("disabled", true);
-    resetButton.classList.add('form__submit-btn_disabled');
-} else {
-    resetButton.removeAttribute("disabled", true);
-    resetButton.classList.remove('form__submit-btn_disabled');
-
-} */
