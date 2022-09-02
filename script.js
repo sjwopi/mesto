d75e89f9c1e009983f2fa62c73110fe4daddd559
Ivan Teteruk <iteterk@gmail.com>
2022-09-02 12:11:58 -0200

    начать делать popup

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ let content = document.querySelector(".content");
 let editButton = content.querySelector(".profile__edit-btn");
 let closeButton = content.querySelector(".popup__close-btn");
 
-
 function editPopUp() {
     let popUp = content.querySelector(".popup");
     popUp.classList.toggle("popup_opened");
 }
 editButton.addEventListener("click", editPopUp);
 closeButton.addEventListener("click", editPopUp);
+
+/* Пробовал лайки добать, но работатет только с первой публикацией, а если я использую querySelectorAll, то вообще не работает */
+
+/* let like = content.querySelectorAll(".element__like");
+function likeActivation() {
+    like.classList.toggle("element__like_active");
+}
+like.addEventListener("click", likeActivation); */
