d4909b8ce4d63dccec5e46445474fcbc0efffe32
Ivan Teteruk <iteterk@gmail.com>
2022-09-13 08:23:18 -0200

    feat: реализованылайки

--- scripts/index.js
+++ scripts/index.js
@@ -21,60 +21,66 @@ const elementsList = document.querySelector('.elements__list');
 
 
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
 ];
 function addPhoto(name, link) {
-    let cardItem = card.querySelector('.elements__list-item').cloneNode(true);
-    cardItem.querySelector('.element__img').src = link;
-    cardItem.querySelector('.element__text').textContent = name;
-    elementsList.prepend(cardItem);
+  let cardItem = card.querySelector('.elements__list-item').cloneNode(true);
+  cardItem.querySelector('.element__img').src = link;
+  cardItem.querySelector('.element__text').textContent = name;
+  elementsList.prepend(cardItem);
+  const likeButton = elementsList.querySelector('.element__like');
+  likeButton.addEventListener('click', likeActivation);
 }
 
 function openPopUpEdit() {
-    nameInputEdit.value = lastName.textContent;
-    jobInputEdit.value = lastDescription.textContent;
-    popUpEdit.classList.toggle('popup_opened');
+  nameInputEdit.value = lastName.textContent;
+  jobInputEdit.value = lastDescription.textContent;
+  popUpEdit.classList.toggle('popup_opened');
 }
-function formSubmitEdit (evt) {
-    evt.preventDefault(); 
-    lastName.textContent = nameInputEdit.value;
-    lastDescription.textContent = jobInputEdit.value;
-    openPopUpEdit();
+function formSubmitEdit(evt) {
+  evt.preventDefault();
+  lastName.textContent = nameInputEdit.value;
+  lastDescription.textContent = jobInputEdit.value;
+  openPopUpEdit();
 }
 
 function openPopUpAdd() {
-    nameInputAdd.value = '';
-    linkInputAdd.value = '';
-    popUpAdd.classList.toggle('popup_opened');
+  nameInputAdd.value = '';
+  linkInputAdd.value = '';
+  popUpAdd.classList.toggle('popup_opened');
 }
-function formSubmitAdd (evt) {
-    evt.preventDefault();
-    initialCards.push({name: nameInputAdd.value, link: linkInputAdd.value});
-    addPhoto(nameInputAdd.value, linkInputAdd.value)
-    openPopUpAdd();
+function formSubmitAdd(evt) {
+  evt.preventDefault();
+  initialCards.push({ name: nameInputAdd.value, link: linkInputAdd.value });
+  addPhoto(nameInputAdd.value, linkInputAdd.value)
+  openPopUpAdd();
+}
+function likeActivation(evt) {
+  const eventTarget = evt.target;
+  eventTarget.classList.toggle('element__like_active');
 }
 
 editButton.addEventListener('click', openPopUpEdit);
@@ -85,6 +91,6 @@ addButton.addEventListener('click', openPopUpAdd);
 closeButtonAdd.addEventListener('click', openPopUpAdd);
 formElementAdd.addEventListener('submit', formSubmitAdd);
 
-for (let i = 0; i < initialCards.length; i++) {
-    addPhoto(initialCards[i].name, initialCards[i].link);
+for (let i = initialCards.length - 1; i >= 0; i--) {
+  addPhoto(initialCards[i].name, initialCards[i].link);
 }
